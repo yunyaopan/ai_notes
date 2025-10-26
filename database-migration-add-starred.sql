07a337e5cbb8466ce844d45ea43f3c1abe534130
Pan Yunyao <timerainy2@gmail.com>
2025-10-26 21:44:23 +0800

    reorganize files

--- database-migration-add-starred.sql
+++
@@ -1,5 +0,0 @@
--- Add starred column to text_chunks table
-ALTER TABLE text_chunks ADD COLUMN IF NOT EXISTS starred BOOLEAN DEFAULT FALSE;
-
--- Create an index on starred for filtering starred chunks
-CREATE INDEX IF NOT EXISTS idx_text_chunks_starred ON text_chunks(starred);
