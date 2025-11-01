0f41d73fa024d4cd40ffe262f9917a5dfbbae91c
Pan Yunyao <timerainy2@gmail.com>
2025-11-01 22:20:37 +0800

    fix price page to be not protected

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -42,7 +42,8 @@ export async function updateSession(request: NextRequest) {
     request.nextUrl.pathname !== '/' &&
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
-    !request.nextUrl.pathname.startsWith('/error')
+    !request.nextUrl.pathname.startsWith('/error') &&
+    !request.nextUrl.pathname.startsWith('/pricing')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
